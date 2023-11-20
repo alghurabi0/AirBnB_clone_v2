495dc8ad5f3e52232391fa5260d36689a93f47a2
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-11-20 23:23:09 +0200

    amen

--- models/place.py
+++ models/place.py
@@ -10,15 +10,14 @@ from models.review import Review
 from models.review import Amenity
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True))
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
 
 
 class Place(BaseModel, Base):
