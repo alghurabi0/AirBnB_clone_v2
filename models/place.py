0655ede9eb765c286de82026cc60cd2203d5229d
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-11-20 21:38:02 +0200

    up

--- models/place.py
+++ models/place.py
@@ -9,17 +9,17 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
     number_bathrooms = Column(Integer, default=0)
+    latitude = Column(Float)
     max_guest = Column(Integer, default=0)
     price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
+    number_rooms = Column(Integer, default=0)
     amenities = relationship("Amenity", secondary="place_amenity",
                              viewonly=False)
     amenity_ids = []
