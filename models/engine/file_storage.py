1b515ba8859bb9cd33955e36c0de5ddfe36257c2
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-11-20 04:26:34 +0200

    xx

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,7 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
-        if type(cls) == str and cls in FileStorage.classes:
+        if cls in FileStorage.classes:
             cls = FileStorage.classes[cls]
             objs = {}
             for key, value in FileStorage.__objects.items():
