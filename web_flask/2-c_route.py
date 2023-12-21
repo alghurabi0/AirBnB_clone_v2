7d64eedbfc0d80e7d8fec32d93e5d160816ca1ef
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-12-21 22:30:44 +0200

    task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000.
+Routes:
+/: display "Hello HBNB!"
+You must use the option strict_slashes=False in your route definition.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Show 'HBNB'"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Show 'C' then text var"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
