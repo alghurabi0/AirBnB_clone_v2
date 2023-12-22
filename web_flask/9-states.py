a346416ac4f246fe93278da80e188298072c19e7
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-12-22 23:02:58 +0200

    task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """get html template for states list"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """get html template for cities by states"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """get html template for states"""
+    states = storage.all(State)
+    if id is None:
+        return render_template('9-states.html', states=states)
+    else:
+        state = storage.get(State, id)
+        if state is not None:
+            print(state)
+            return render_template('9-states.html', state=state)
+        else:
+            return render_template('9-states.html', id="Not found!")
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """close storage"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
