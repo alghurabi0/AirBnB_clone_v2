e337efc47ba6cd538c464764f71e05ed6ea1ae50
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-12-22 22:21:05 +0200

    fixx

--- models/place.py
+++ models/place.py
@@ -23,7 +23,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     user_id = Column(String(60, collation="latin1_swedish_ci"), ForeignKey("users.id"), nullable=False)
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(60, collation="utf8mb4_0900_ai_ci"), ForeignKey("cities.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_bathrooms = Column(Integer, default=0)
